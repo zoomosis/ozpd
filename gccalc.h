11aa2efd83c70db2c8f0b5966efee6c9153e6392
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:56:10 +1100

    Initial import

---
+++ gccalc.h
@@ -0,0 +1,6 @@
+/* released to the public domain */
+
+double degdist(double lat1, double lon1, double lat2, double lon2);
+double raddist(double lat1, double lon1, double lat2, double lon2);
+double d2r(double deg);
+double r2m(double rad);
