fe051533247b00b298c35fd676197a540137f2d0
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:50:34 +1100

    Initial import

--- md5.h
+++ md5.h
@@ -1,10 +1,10 @@
 #ifndef MD5_H
 #define MD5_H
 
-#if SIZEOF_LONG == 4
+#if sizeof(long) == 4
 typedef unsigned long uint32;
 #else
-#if SIZEOF_INT == 4
+#if sizeof(int) == 4
 typedef unsigned int uint32;
 #else
 Congratulations!  You get to rewrite this code so that it does not require
@@ -18,10 +18,10 @@ struct MD5Context {
 	unsigned char in[64];
 };
 
-void MD5Init PROTO((struct MD5Context *context));
-void MD5Update PROTO((struct MD5Context *context, unsigned char const *buf, unsigned len));
-void MD5Final PROTO((unsigned char digest[16], struct MD5Context *context));
-void MD5Transform PROTO((uint32 buf[4], uint32 const in[16]));
+void MD5Init(struct MD5Context *context);
+void MD5Update(struct MD5Context *context, unsigned char const *buf, unsigned len);
+void MD5Final(unsigned char digest[16], struct MD5Context *context);
+void MD5Transform(uint32 buf[4], uint32 const in[16]);
 
 /*
  * This is needed to make RSAREF happy on some MS-DOS compilers.
