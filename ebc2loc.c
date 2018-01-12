8f0b5461a0fcae716ebef16d5cb5c4e9bd20773a
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:58:54 +1100

    Initial import

--- ebc2loc.c
+++ ebc2loc.c
@@ -7,13 +7,15 @@
 /*********************************************************************/
 /*                                                                   */
 /*  This program converts an ebcdic file into the local character    */
-/*  set.                                                             */
+/*  set. It will insert line breaks at the end of a record (default  */
+/*  80) but does not trim spaces.                                    */
 /*                                                                   */
 /*********************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "febc.c"
+#include "febc.h"
 
 int main(int argc, char **argv)
 {
@@ -21,22 +23,35 @@ int main(int argc, char **argv)
     FILE *fq;
     int c;
     int x = 0;
+    int lrecl = 80;
     
     if (argc <= 2)
     {
-        printf("usage: ebc2loc infile outfile\n");
-        return (0);
+        printf("usage: ebc2loc infile outfile [record length]\n");
+        return (EXIT_FAILURE);
     }
     
     fp = fopen(*(argv + 1), "rb");
-    fq = fopen(*(argv + 2), "wb");
+    fq = fopen(*(argv + 2), "w");
+    if (argc > 3)
+    {
+        lrecl = atoi(*(argv + 3));
+    }
     
     while ((c = fgetc(fp)) != EOF)
     {
+        int origc;
+        
         x++;
+        origc = c;
         c = febc(c);
+        if (c == 0)
+        {
+            printf("converting unknown character x'%2X' to space\n", origc);
+            c = ' ';
+        }
         fputc(c, fq);
-        if (x == 1000)
+        if (x == lrecl)
         {
             fputc('\n', fq);
             x = 0;
