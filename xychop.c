8b818f65e5a18a628c68a3a0c8458287e4b29de9
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:56:10 +1100

    Initial import

--- xychop.c
+++ xychop.c
@@ -1,9 +1,13 @@
-/* Written by Xie Yi Zhong */
+/* Written by Raymond Zhong */
 /* Released to the public domain */
 /* modified 1995-02-05 by Paul Edwards to make it zero-based and
    not prompt for overwrite of output file */
 /* modified 1995-07-23 by Paul Edwards to make it use longs when
-   accessing the file.  Also fixed a couple of bugs. */   
+   accessing the file.  Also fixed a couple of bugs. */
+/* modified 1999-08-10 by Paul Edwards to fix zero-based
+   processing and other cosmetic changes. */
+/* modified 1999-08-19 by Paul Edwards to use more flexible
+   strtol instead of atol. */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,57 +20,69 @@ int main(int argc, char *argv[])
     
     if (argc != 5)
     {
-        fprintf(stderr, 
-                "\n\aUsage: %s infile outfile startch endch \n\n", 
-                argv[0]);
+        printf("Usage: %s infile outfile startch endch\n", 
+               argv[0]);
         return(EXIT_FAILURE);
     }
 
     if ((infile = fopen(argv[1],"rb")) == NULL)
     {
-        fprintf(stderr,"\n\aUnable to open infile '%s'\n\n", argv[2]);
+        printf("Unable to open infile '%s'\n", argv[2]);
         return(EXIT_FAILURE);
     }
 
-    outfile = fopen(argv[2], "wb");
+    if ((outfile = fopen(argv[2], "wb")) == NULL)
+    {
+        printf("Unable to open outfile '%s'\n", argv[3]);
+        return (EXIT_FAILURE);
+    }
 
-    startch = atol(argv[3]);        
-    endch = atol(argv[4]);
+    startch = strtol(argv[3], NULL, 0);
+    endch = strtol(argv[4], NULL, 0);
+    
     if (startch > endch)
     {
-        printf("\n\aStart position can not be greater "
-                "than end position.");
+        printf("Start position can not be greater "
+                "than end position.\n");
         return(EXIT_FAILURE);
     }
 
-    if ((in_ch = getc(infile)) != EOF)
+    /* skip leading characters */
+    while ((count < startch) && ((in_ch = getc(infile)) != EOF))
     {
         count++;
-        while ((count < startch) && (in_ch != EOF))
+    }
+    
+    if (in_ch == EOF)
+    {
+        printf("End of file before startch.\n");
+        return(EXIT_FAILURE);
+    }
+    
+    /* copy remaining characters */
+    while ((count <= endch) && ((in_ch = getc(infile)) != EOF))
+    {
+        if (putc(in_ch, outfile) == EOF)
         {
-            in_ch = getc(infile);
-            count++;
+            printf("write error\n");
+            return (EXIT_FAILURE);
         }
-        if (in_ch == EOF)
-        {
-            printf("\n\aEnd of file before startch.");
-            return(EXIT_FAILURE);
-        }
-        while ((count <= endch) && (in_ch != EOF))
-        {
-            putc(in_ch, outfile);
-            in_ch = getc(infile);
-            count++;
-        }
-        if (in_ch == EOF)
-        {
-            printf("\n\aEnd of file before endch.");
-            return(EXIT_FAILURE);
-        }        
+        count++;
     }
 
+    if (ferror(infile))
+    {
+        printf("error occurred reading file\n");
+        return (EXIT_FAILURE);
+    }
+    
     fclose(infile);
-    fclose(outfile);
+    
+    if (fclose(outfile) == EOF)
+    {
+        printf("failed to close output file\n");
+        return (EXIT_FAILURE);
+    }
 
-    return(0);
+    return (EXIT_SUCCESS);
 }
