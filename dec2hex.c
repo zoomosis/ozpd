1cbedea13e2688ec6a637cdc4dc270d134171c20
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:56:11 +1100

    Initial import

---
+++ dec2hex.c
@@ -0,0 +1,28 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  dec2hex - convert a decimal number into hex                      */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    long x;
+    
+    if (argc < 2)
+    {
+        printf("usage: dec2hex <n>\n");
+        return (EXIT_FAILURE);
+    }
+    x = strtol(*(argv+1), NULL, 10);
+    printf("%lx\n", x);
+    return (EXIT_SUCCESS);
+}
