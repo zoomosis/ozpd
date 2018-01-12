3ae846234b1fb398037007bb80ebf0229d6706c1
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:56:11 +1100

    Initial import

---
+++ hex2dec.c
@@ -0,0 +1,28 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  hex2dec - convert a hex number into decimal                      */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    long x;
+    
+    if (argc < 2)
+    {
+        printf("usage: hex2dec <n>\n");
+        return (EXIT_FAILURE);
+    }
+    x = strtol(*(argv+1), NULL, 16);
+    printf("%ld\n", x);
+    return (EXIT_SUCCESS);
+}
