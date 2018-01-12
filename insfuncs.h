b1cb91358e651400f1d46abd26c3d10118e5edde
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 00:56:11 +1100

    Initial import

---
+++ insfuncs.h
@@ -0,0 +1,4 @@
+/* public domain by Frank Adam */
+int InsChar(char *point, char ch);
+int InsStr(char *txt, char *point, char *str, size_t maxlen);
+int InsWord(char *txt, char *point, char *word, size_t maxlen);
